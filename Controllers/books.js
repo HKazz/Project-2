42dc8d7b01527fc6f35b6ede0d18b77a3bffe37b
RashidAlsabt <rashidalsabt@outlook.com>
2025-01-22 15:08:53 +0300

    Copied changes from Ahmed/new-book-form-and-route

--- Controllers/books.js
+++ Controllers/books.js
@@ -1,3 +1,19 @@
 const router = require("express").Router()
 const Books = require('../models/books')
 
+router.get('/new', (req,res)=>{
+    res.render('books/new.ejs', {user: req.session.user})
+})
+router.post('/', async (req,res)=>{
+    try {
+        console.log(req.session.user)
+        req.body.creator = req.session.user._id
+        console.log(req.body)
+        const createdBook = await Book.create(req.body)
+        res.redirect('/books')
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+module.exports = router
